24dec57b908e5ac95fe76888b89c5457c3c28a9c
Adama Dabo <dabodabo9064@gmail.com>
2024-06-10 12:00:36 +0000

    Changement de mon nom d'entreprise

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -144,7 +144,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container">
-            <a class="navbar-brand" href="#">DIWAN TECH</a>
+            <a class="navbar-brand" href="#">Kane & Frères</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
